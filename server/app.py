596fc6eb8754da2333ef4047f0698e766dfbcd6a
Enoch2k2 <enoch2k2@hotmail.com>
2023-08-07 16:22:58 -0500

    initialized db

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from config import app
+from config import app, db
 
 
 if __name__ == "__main__":
---
+++ server/migrations/versions/543842ab36a6_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 543842ab36a6
+Revises: 
+Create Date: 2023-08-07 16:22:31.674766
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '543842ab36a6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
